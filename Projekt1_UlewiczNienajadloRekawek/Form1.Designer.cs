fffefb82aaf641716dfa7fb58d7f84cc16a2c0ee
Boldeusz <mnpower@poczta.onet.pl>
2017-10-30 17:20:29 +0100

    poprawka

--- Projekt1_UlewiczNienajadloRekawek/Form1.Designer.cs
+++ Projekt1_UlewiczNienajadloRekawek/Form1.Designer.cs
@@ -28,33 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(25, 31);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-=======
-            this.SuspendLayout();
-            // 
->>>>>>> d717df495a6201584057ae96c9ef8fdcf93cbb52
             // UNRGlowne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(608, 395);
-            this.Controls.Add(this.button1);
-=======
-            this.ClientSize = new System.Drawing.Size(576, 380);
->>>>>>> d717df495a6201584057ae96c9ef8fdcf93cbb52
             this.Name = "UNRGlowne";
             this.Text = "Analizator algorytmów sortowania";
             this.ResumeLayout(false);
@@ -62,11 +39,7 @@
         }
 
         #endregion
-<<<<<<< HEAD
 
-        private System.Windows.Forms.Button button1;
-=======
->>>>>>> d717df495a6201584057ae96c9ef8fdcf93cbb52
     }
 }
 
